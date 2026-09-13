186e144e44bf2ef0f28ed80403e41b2f99bde2f9
SerGGrad007 <serggrad007@mail.ru>
2026-09-13 16:54:04 +0300

    Class User: fixing inheritance

--- include/User.h
+++ include/User.h
@@ -1,6 +1,5 @@
 #pragma once
 
-#ifndef LIBRARY_MANAGER_USER_H
 #define LIBRARY_MANAGER_USER_H
 
 #include <string>
@@ -12,72 +11,76 @@ class User {
 public:
     User(std::string name, std::string userId, std::string email):
     name(std::move(name)), userId(std::move(userId)), email(std::move(email)){
-        this->borrowedBooks = new std::vector<std::string>();
+        this->borrowedBooks = std::vector<std::string>();
     }
 
-    int getMaxBooks();
-    int getBorrowDays();
-    double getFinePerDay();
+    virtual int getMaxBooks() = 0;
+    virtual int getBorrowDays() = 0;
+    virtual double getFinePerDay() = 0;
+
+    virtual ~User();
 
     bool canBorrow() {
-        return borrowedBooks->size() < getMaxBooks();
+        return borrowedBooks.size() < static_cast<size_t>(getMaxBooks());
     }
 
 protected:
     std::string name;
     std::string userId;
     std::string email;
-    std::vector<std::string>* borrowedBooks;
+    std::vector<std::string> borrowedBooks;
 };
 
-class Student : public User {
+class Student final : public User {
+public:
     Student(std::string name, std::string userId, std::string email):
     User(std::move(name), std::move(userId), std::move(email)){}
 
-    int getMaxBooks() {
+    int getMaxBooks() override {
         return 3;
     }
 
-    int getBorrowDays() {
+    int getBorrowDays() override {
         return 14;
     }
 
-    double getFinePerDay() {
+    double getFinePerDay() override {
         return 0.50;
     }
 };
 
-class Faculty : public User {
+class Faculty final : public User {
+public:
     Faculty(std::string name, std::string userId, std::string email):
     User(std::move(name), std::move(userId), std::move(email)){}
 
-    int getMaxBooks() {
+    int getMaxBooks() override {
         return 10;
     }
 
-    int getBorrowDays() {
+    int getBorrowDays() override {
         return 30;
     }
 
-    double getFinePerDay() {
+    double getFinePerDay() override {
         return 0.50;
     }
 };
 
-class Guest : public User {
+class Guest final : public User {
+public:
     Guest(std::string name, std::string userId, std::string email):
     User(std::move(name), std::move(userId), std::move(email)){}
 
-    int getMaxBooks() {
+    int getMaxBooks() override {
         return 1;
     }
 
-    int getBorrowDays() {
+    int getBorrowDays() override {
         return 7;
     }
 
-    double getFinePerDay() {
+    double getFinePerDay() override {
         return 0.50;
     }
 };
-#endif //LIBRARY_MANAGER_USER_H
